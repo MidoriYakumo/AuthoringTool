a768e1e1c40d1f5d22ca623213a741b2093dd59f
Liang Peng <itoupeter@gmail.com>
2016-03-18 21:27:38 -0400

    load image; load model;

---
+++ maya/AutomaticRigging/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+
+#include "mesh.h"
+
+int main(){
+
+	string filename( "body.obj" );
+	Mesh mesh( filename );
+
+
+
+	return 0;
+}
--- maya/MayaPlugin/PluginMain.cpp
+++ maya/MayaPlugin/PluginMain.cpp
@@ -9,18 +9,18 @@
 MStatus initializePlugin( MObject obj )
 {
     MStatus   status = MStatus::kSuccess;
-    MFnPlugin plugin( obj, "Body Reshaper", "1.0", "1.0");
+    MFnPlugin plugin( obj, "Figure Reshaper", "1.0", "1.0" );
 
     //---register command---
-    status = plugin.registerCommand( "BodyReshaper", BodyReshaper::creator, BodyReshaper::newSyntax );
+    status = plugin.registerCommand( "FigureReshaper", BodyReshaper::creator, BodyReshaper::newSyntax );
 	if (!status){ status.perror( "registerCommand" ); return status; }
 	
 	//---create menu---
 	MString cmd = "global string $gMainWindow; \n"
 		"setParent $gMainWindow; \n"
-		"menu -label \"Body Reshaper\" myBodyReshaperMenu; \n"
-		"setParent -menu myBodyReshaperMenu; \n"
-		"menuItem -label \"Body Reshaper\" -command \"source \\\""
+		"menu -label \"Figure Reshaper\" myFigureReshaperMenu; \n"
+		"setParent -menu myFigureReshaperMenu; \n"
+		"menuItem -label \"Figure Reshaper\" -command \"source \\\""
 		+ plugin.loadPath() + "/MyPluginDialog\\\"\"; \n";
 
 	MGlobal::executeCommand( cmd, true );
@@ -34,11 +34,11 @@ MStatus uninitializePlugin( MObject obj )
     MFnPlugin plugin( obj );
 
 	//---deregister command---
-    status = plugin.deregisterCommand( "BodyReshaper" );
+    status = plugin.deregisterCommand( "FigureReshaper" );
 	if (!status){ status.perror( "deregisterCommand" ); return status; }
 
 	//---delete menu---
-	MGlobal::executeCommand( "deleteUI myBodyReshaperMenu;" );
+	MGlobal::executeCommand( "deleteUI myFigureReshaperMenu;" );
 
     return status;
 }
